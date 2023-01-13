52f789c52217181ec225292b7a75cdd8379fb8d6
Fernando Avalos <fernandoadev@protonmail.com>
2023-01-13 16:57:05 -0500

    First stage of model

---
+++ model/interface.py
@@ -0,0 +1,9 @@
+class Dataset:
+
+    def __init__(self, train_data, train_labels, test_data,  test_labels):
+        self.train_data = train_data
+        self.train_labels = train_labels
+        self.test_data = test_data
+        self.test_labels = test_labels
+
+        
